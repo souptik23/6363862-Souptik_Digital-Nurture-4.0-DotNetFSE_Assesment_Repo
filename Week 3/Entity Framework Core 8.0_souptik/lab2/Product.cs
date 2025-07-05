d3f9533bcd5490118c95ff57247af8d7805f6510
souptik23 <souptikkaran23@gmail.com>
2025-07-06 02:40:11 +0530

    week 3 part 2 remaining part done and the readme file

---
+++ Week 3/Entity Framework Core 8.0_souptik/lab2/Product.cs
@@ -0,0 +1,16 @@
+
+namespace MyFirstConsoleApp_6363862.Models
+{
+    public class Product
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; } = string.Empty;
+
+        public decimal Price { get; set; }
+
+        public int CategoryId { get; set; }
+
+        public Category Category { get; set; }
+    }
+}
